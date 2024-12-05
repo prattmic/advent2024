82503248a256a70af9207a4981547021ce146332
Michael Pratt <michael@pratt.im>
2024-12-04 22:45:52 -0500

    dec01: start reading file

--- src/bin/dec01/main.rs
+++ src/bin/dec01/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} input.txt", args[0]);
+        return ExitCode::from(1);
+    }
+
+    let file = &args[1];
+
+    let contents = fs::read_to_string(file).expect("error reading file");
+
+    println!("Read {} bytes", contents.len());
+
+    ExitCode::SUCCESS
 }
